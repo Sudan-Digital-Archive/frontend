e0aeb6b314aa09afd3cd9739e499baedef4142d3
zhabiba24 <182411820+zhabiba24@users.noreply.github.com>
2026-05-02 13:01:08 -0400

    chore: remove thumb

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -90,7 +90,6 @@ export default function Collections() {
               >
                 <Switch.HiddenInput />
                 <Switch.Control />
-                <Switch.Thumb />
               </Switch.Root>
             </Flex>
           )}
